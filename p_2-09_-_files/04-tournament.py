a2951899c566190f5274b108ce7f02ce9f37d0ba
Sergali Bakirov <bakirovdvm@gmail.com>
2024-11-13 00:09:08 +0600

    p_2-09_-_files - done

--- p_2-09_-_files/04-tournament.py
+++ p_2-09_-_files/04-tournament.py
@@ -9,25 +9,49 @@ file = open('first_tour.txt', 'r', encoding='utf-8')
 
 data = list()
 for item in file:
-    print(item, end='')
+    # print(item, end='')
     data.append(item.replace('\n', '').split())
 
 file.close()
-print()
-print(data)
 
-result_file = open('second_tour.txt', 'w')
+print('data:', data)
+
+result_list = list()
 count = 0
 for item in range(1, len(data)):
     info = str()
-    if int(data[0][0]) < int(data[item][2]):
-        info = f'{data[item][1][:1]}. {data[item][0]} {data[item][2]}\n'
-        result_file.write(info)
-        count += 1
 
-result_file.write(count)
+    if int(data[0][0]) < int(data[item][2]):
+        info = f'{data[item][1][:1]}. {data[item][0]} {data[item][2]}'
+        result_list.append(info)
+
+print('\nresult_list'.upper(), result_list)
+
+list_for_sort = list()
+for i in range(len(result_list)):
+    # print(result_list[i].split())
+    list_for_sort.append(result_list[i].split())
+
+list_sorted = sorted(list_for_sort, key=lambda winner:int(winner[2]), reverse=True)
+# print(list_sorted)
+
+result = list()
+result.append(0)
+
+for item in list_sorted:
+    result.append(item)
+
+result[0] = len(list_sorted)
+# print(result)
+
+result_file = open('second_tour.txt', 'w', encoding='utf-8')
+for i in result:
+    data = f'{str(i)}\n'
+    result_file.write(data)
 result_file.close()
 
-result = open('second_tour.txt', 'r', encoding='utf-8')
-for i in result:
-    print(i, end='')
+result_data = open('second_tour.txt', 'r', encoding='utf-8')
+print('\nСодержимое файла second_tour.txt:')
+for item in result_data:
+    print(item, end='')
+result_data.close()
