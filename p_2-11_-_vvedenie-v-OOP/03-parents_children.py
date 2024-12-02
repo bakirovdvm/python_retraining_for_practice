8d829394c84b7ce5cb201e3fdafe3275f26320a4
Sergali Bakirov <bakirovdvm@gmail.com>
2024-12-03 00:13:17 +0600

    p_2-11_-_OOP - parents is done

---
+++ p_2-11_-_vvedenie-v-OOP/03-parents_children.py
@@ -0,0 +1,120 @@
+# class Parent:
+#     def __init__(self, name, age, children_list):
+#         self.name = name
+#         self.age = age
+#         self.children_list = children_list
+#
+#     def print_info(self):
+#         print('my name is {}, \n my age is {}, \nmy children: {}'.format(self.name,
+#                                                                          self.age,
+#                                                                          self.children_list))
+#
+#
+#     def calm_Child(self):
+#         Child.calm = True
+#
+#
+#     def fed_child(self):
+#         Child.hungry = False
+#
+#
+# class Child:
+#     calm = False
+#     hungry = True
+#
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         # self.calm = True
+#         # self.hungry = True
+#
+
+class Parent:
+    def __init__(self, name, age, child_quantity):
+        self.name = name
+        self.age = age
+        self.child_quantity = child_quantity
+        self.children_list = []
+
+    def print_info(self):
+        print('Меня зовут:', self.name)
+        print('Мне {} лет'.format(self.age))
+        print('У меня {} детей: {}'.format(len(self.children_list), self.children_list))
+
+    def give_food(self, id_child, action):
+        self.id_child = id_child
+        self.action = action
+        for child in range(1, len(self.children_list) + 1):
+            if self.id_child == child:
+                self.children_list[child-1].state_of_hungry = action
+                # self.children_list[child].print_info()
+
+    def give_relax(self, id_child, action):
+        self.id_child = id_child
+        self.action = action
+        for child in range(1, len(self.children_list) + 1):
+            if self.id_child == child:
+                self.children_list[child-1].state_of_relax = action
+
+
+class Child:
+    state_of_relax_ = {1: 'спокоен ', 0: 'плачет '}
+    state_of_hungry_ = {1: 'сыт ', 0: 'голоден '}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.state_of_relax = 0
+        self.state_of_hungry = 0
+
+    def print_info(self):
+        print('Имя ребенка:', self.name)
+        print('Возраст', self.age)
+        print('Ребенок спокоен?', Child.state_of_relax_[self.state_of_relax])
+        print('Ребенок голоден?', Child.state_of_hungry_[self.state_of_hungry])
+        print()
+
+
+papa = Parent('Папа', 36, 2)
+
+
+for child in range(1, papa.child_quantity + 1):
+    child = Child(input('\nВведите имя {} ребенка: '.format(child)),
+                  int(input('Сколько {} ребенку лет? '.format(child))))
+    papa.children_list.append(child)
+
+
+errors = []
+for child in range(len(papa.children_list)):
+    if (papa.age - 16) <= papa.children_list[child].age:
+        errors.append('error')
+
+
+if 'error' in errors:
+    print('\nИнформация о возрасте ребенка не верна! перепроверьте!')
+else:
+    print('\nПапа кормит и успокаивает своих детей:')
+    papa.give_food(1, 1)
+    papa.give_relax(1, 1)
+    papa.give_food(2, 1)
+    papa.give_relax(2, 1)
+
+    for i in papa.children_list:
+        i.print_info()
+
+
+    print('\nПапа кормит и нервирует своих детей:')
+    papa.give_food(1, 1)
+    papa.give_relax(1, 0)
+    papa.give_food(2, 1)
+    papa.give_relax(2, 0)
+
+    for child in papa.children_list:
+        child.print_info()
+
+
+
+
+
+
+
